8f0256df470c952f3c45bcedb631f64c8ea042ae
Gagan <business@devgagan.in>
2025-01-23 21:56:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------
+# File Name: config.py
+# Description: A Aiogram Wrapper for Save Restricted Content Bot
+# Copyright: devgagan
+# GitHub: https://github.com/devgaganin/
+# Telegram: https://t.me/team_spy_pro
+# YouTube: https://youtube.com/@dev_gagan
+# Version: 1.0.0
+# -------------------------------------------------
+
+from os import getenv
+
+BOT_TOKEN = getenv("BOT_TOKEN", "")

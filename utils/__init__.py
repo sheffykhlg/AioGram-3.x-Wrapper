78518024d4b8fc907dd2b98a2a169870da6481d2
Gagan <business@devgagan.in>
2025-01-23 21:59:38 +0530

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,9 @@
+# ---------------------------------------------------
+# File Name: __init__.py
+# Description: A Aiogram Wrapper for Save Restricted Content Bot
+# Copyright: devgagan
+# GitHub: https://github.com/devgaganin/
+# Telegram: https://t.me/team_spy_pro
+# YouTube: https://youtube.com/@dev_gagan
+# Version: 1.0.0
+# ---------------------------------------------------

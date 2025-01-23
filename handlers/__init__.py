e24a9b5d40bd818fb0014a3a0b7bdf1df6bd6d71
Gagan <business@devgagan.in>
2025-01-23 22:01:01 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,14 @@
+# ---------------------------------------------------
+# File Name: __init__.py
+# Description: A Aiogram Wrapper for Save Restricted Content Bot
+# Copyright: devgagan
+# GitHub: https://github.com/devgaganin/
+# Telegram: https://t.me/team_spy_pro
+# YouTube: https://youtube.com/@dev_gagan
+# Version: 1.0.0
+# ---------------------------------------------------
+
+
 from .start import router as start_router
 from .help import router as help_router
 from .echo import router as echo_router
